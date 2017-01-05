fd267df15c61d0cec0fb00dd16a9c387a01f0ea9
brito <francisco.brito@oracle.com>
2017-01-04 21:02:55 -0800

    Remove 'interpolate' to html... not sure what the UX should be!

--- test.js
+++ test.js
@@ -7,11 +7,8 @@ document.addEventListener('DOMContentLoaded', function(){
 		// 1. index browser bookmarks
 		var bookmarks = results[0].children.reduce(index, {});
 		console.info(bookmarks);
-		// 2. interpolate data into html
-		document.body.appendChild(interpolate(bookmarks));
-		throw 'TODO: append table with data'
 	});
-	
+
 });
 
 /* index */
@@ -24,10 +21,8 @@ function index(Bookmarks, bookmark){
 			.forEach(function(key){ 
 				bookmark[key] = url[key];
 			});
-	}
-
-	if (bookmark.host){
-		bookmark.domain = (bookmark.host.match(/[^\.]+\.\w+$/) || [''])[0];
+		bookmark.domain = 
+			(bookmark.host.match(/[^\.]+\.\w+$/) || [''])[0];
 	}
 
 	for (var key in bookmark){ 
@@ -36,33 +31,19 @@ function index(Bookmarks, bookmark){
 		if (value instanceof Array)
 			// flatten
 			Bookmarks = value.reduce(index, Bookmarks);
-		else {
+		else
 			Bookmarks[key] = archive(Bookmarks[key], value, bookmark);
-		}
 	}
+
 	return Bookmarks;
 }
 
-/* function archive */
+/* archive */
 function archive(context, value, item){
 	context = context || {};
 	context[value] = value in context ?
 		[item].concat(context[value])
 		: item;
 	return context;
-}
-
-/* interpolate */
-function interpolate(data){
-	var thead = document.createElement('thead'),
-		tbody = document.createElement('tbody')
-	for (var column in data){
-		var th = document.createElement('th'),
-			td = document.createElement('td');
-		th.innerText = column;
-		td.innerText = data[column];
-		thead.appendChild(th);
-	}
-	return document.createElement('table');
 }
 
